6d8b68101fd74dfb510b34b0d8f78bc59e6c5224
Rajkumarpaneru18 <rajkumarpaneru111@gmail.com>
2024-04-09 11:24:21 +0545

    added landing page

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,23 @@
+import Features from "../Components/Features";
+import FeedbackForm from "../Components/FeedbackForm/FeedbackForm";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import Hero from "../Components/Hero";
+
+import Layout from "../Layout/Layout";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Layout>
+        <Header />
+        <Hero />
+        <Features />
+        <FeedbackForm />
+        <Footer />
+      </Layout>
+    </>
+  );
+};
+
+export default LandingPage;
